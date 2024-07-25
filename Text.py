5f4ebfa1d88c3654faf8eb45a74cb02ed1267c38
Nataly <natalia@base>
2024-07-25 17:21:14 +0200

    add text cleansing and language choose

--- Text.py
+++ Text.py
@@ -2,20 +2,57 @@ import pytesseract
 from PIL import Image
 import fitz
 import io
+import re
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+
+nltk.download('wordnet')
+
+lemmatizer = WordNetLemmatizer()
 
 
 class TextTransformer:
+
     def __init__(self, file_bytes):
         self.file_bytes = file_bytes
 
-    def extract_text_from_image(self):
+    def extract_text_from_image(self, lang="eng+rus"):
         image = Image.open(io.BytesIO(self.file_bytes))
-        text = pytesseract.image_to_string(image, lang='rus')
+        text = pytesseract.image_to_string(image, lang)
         return text
 
     def extract_text_from_pdf(self):
-        text = ""
+        text = ''
         pdf_document = fitz.open(stream=self.file_bytes, filetype="pdf")
         for page in pdf_document:
             text += page.get_text()
         return text
+
+    @staticmethod
+    def clean_text(text):
+        # Удалить специальные символы
+        text = re.sub(r'[^\w\s]', '', text)
+
+        text = ' '.join(text.split())
+
+        # Токенизация текста
+        words = nltk.word_tokenize(text)
+
+        # Лемматизация
+        lemmatized_words = [lemmatizer.lemmatize(word) for word in words]
+        cleaned_text = ' '.join(lemmatized_words)
+
+        return cleaned_text
+
+    def process_file(self, file_type, languages="eng+rus"):
+        if file_type == "application/pdf":
+            text = self.extract_text_from_pdf()
+        elif file_type.startswith("image/"):
+            text = self.extract_text_from_image(languages)
+        else:
+            raise ValueError("Unsupported file type")
+
+        cleaned_text = self.clean_text(text)
+
+        return cleaned_text
--- main.py
+++ main.py
@@ -1,20 +1,41 @@
-from fastapi import FastAPI, File, UploadFile, HTTPException
+import fastapi
+from fastapi import FastAPI, File, UploadFile, HTTPException, Form
+
+from fastapi.responses import StreamingResponse
+
+from enum import Enum
+import os
 import Text
 
-app = FastAPI()
+app = FastAPI(
+    title="Text Extraction API",
+    description="API для извлечения текста из изображений и PDF файлов",
+    version="1.0.0"
+)
 
 
-@app.post("/extract-text")
-async def extract_text(file: UploadFile = File(...)):
+class SupportedLanguages(str, Enum):
+    eng = "eng"
+    rus = "rus"
+    eng_rus = "eng+rus"
+
+
+@app.post("/extract-text", summary="Извлечение текста из файла", tags=["Text Extraction"])
+async def extract_text(
+        file: UploadFile = File(...),
+        languages: SupportedLanguages = Form(SupportedLanguages.eng_rus)
+
+):
     content = await file.read()
+    file_type = file.content_type
     pict = Text.TextTransformer(content)
 
-    if file.content_type == "application/pdf":
-        text = pict.extract_text_from_pdf()
+    if file_type == "application/pdf":
+        text = pict.process_file(file_type)
         response = {'file_type': 'PDF', 'content': text}
 
-    elif file.content_type.startswith("image/"):
-        text = pict.extract_text_from_image()
+    elif file_type.startswith("image/"):
+        text = pict.process_file(file_type, languages)
         response = {'file_type': 'IMAGE', 'content': text}
 
     else:
